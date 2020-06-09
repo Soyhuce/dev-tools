b617b7687ffde37f945918d99b6898d604c8d8de
Bastien Philippe <bastien.philippe@soyhuce.fr>
2020-06-09 16:25:05 +0200

    Refactor ResponseCollector

--- src/Debug/Collectors/ResponseCollector.php
+++ src/Debug/Collectors/ResponseCollector.php
@@ -2,62 +2,39 @@
 
 namespace Soyhuce\DevTools\Debug\Collectors;
 
-use Carbon\Carbon;
 use Soyhuce\DevTools\Debug\Collectors\Concerns\RegistersDebugMiddleware;
+use Soyhuce\DevTools\Debug\Entries\Entry;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
-/**
- * Class ResponseCollector
- */
 class ResponseCollector extends DataCollector
 {
     use RegistersDebugMiddleware;
 
-    /** @var array */
-    private $data;
-
-    /**
-     * ResponseCollector constructor.
-     */
-    public function __construct()
-    {
-        $this->data = [];
-    }
+    private ?Entry $entry = null;
 
     public function getName(): string
     {
         return 'response';
     }
 
-    public function isEnabled(): bool
-    {
-        return config('dev-tools.debugger.response.enabled');
-    }
-
     public function collect(): array
     {
-        return $this->data
-            ? [
-                [
-                    'time' => $this->data['time'],
-                    'pretty_time' => Carbon::createFromTimestamp((int) $this->data['time'])->toDateTimeString(),
-                    'message' => $this->data['status'] . ' -> ' . $this->data['content'],
-                    'type' => $this->getName(),
-                ],
-            ]
-            : [];
+        if ($this->entry === null) {
+            return [];
+        }
+
+        return [$this->entry];
     }
 
-    public function setResponse(Response $response)
+    public function setResponse(Response $response): void
     {
-        $this->data = [
-            'time' => $this->time(),
-            'status' => $response->getStatusCode(),
-            'content' => $this->getContent($response),
-        ];
+        $this->entry = new Entry(
+            $this->getName(),
+            "{$response->getStatusCode()} -> {$this->getContent($response)}"
+        );
     }
 
     private function getContent(Response $response)
@@ -72,6 +49,6 @@ class ResponseCollector extends DataCollector
             return 'redirect to ' . $response->getTargetUrl();
         }
 
-        return $response->getContent();
+        return PHP_EOL . $response->getContent();
     }
 }
