932bd59e98eb8050b4d188ed75a94ea502689d23
Bastien Philippe <bastien.philippe@soyhuce.fr>
2020-06-09 10:02:44 +0200

    Refactor BottleneckMiddleware, update readme

---
+++ src/Middlewares/BottleneckMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Soyhuce\DevTools\Middlewares;
+
+use Closure;
+
+class BottleneckMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $this->sleep($request);
+
+        return $next($request);
+    }
+
+    private function sleep($request): void
+    {
+        if (config('dev-tools.bottleneck.only_ajax') && !$request->ajax()) {
+            return;
+        }
+
+        usleep(config('dev-tools.bottleneck.duration') * 1000);
+    }
+}
